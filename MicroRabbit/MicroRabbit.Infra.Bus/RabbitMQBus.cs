dd42596ee3cb5b83c7ecdd8fe60628048e7ea98e
IoaMertz <119110449+IoaMertz@users.noreply.github.com>
2023-04-27 17:47:36 +0300

    Started making MicroRabbitMQBus (MINE!!)

---
+++ MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,32 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.Bus
+{
+    //sealed we dont want any1 to inherit or extend this class == sealed
+    public sealed class RabbitMQBus : IEventBus
+    {
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish<T>(T @event) where T : Event
+        {
+            throw new NotImplementedException();
+        }
+
+        public void subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
