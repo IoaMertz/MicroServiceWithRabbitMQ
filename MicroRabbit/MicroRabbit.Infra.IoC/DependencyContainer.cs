01151e65790a035ab303914498c085fc2a8925ad
IoaMertz <119110449+IoaMertz@users.noreply.github.com>
2023-05-28 10:54:30 +0300

    Initial commit of infrastructure Dependancy Project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+
+    }
+}
